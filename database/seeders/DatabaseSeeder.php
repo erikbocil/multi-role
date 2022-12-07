3a8b643e03e07b32471be1feb17378c6de6c763a
erikbocil <erikluis3006@gmail.com>
2022-12-08 00:50:44 +0700

    Initial Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'student']);
+        Role::create(['name' => 'teacher']);
+        Role::create(['name' => 'admin']);
+
+        User::create([
+            'name' => 'admin123',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role_id' => 3,
+        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
